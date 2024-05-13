f77195581abf8dae67b2e82fc9ae1dba0f4ef964
Ronel De Jesus <r.dejesus@lionheart.asia>
2024-05-13 13:08:33 +0800

    added new files

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ import linkRoute from './route/linkRoute.js';
 import errorHandler from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 connectDB();
@@ -11,11 +13,22 @@ connectDB();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// resolving dirname for ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__dirname);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use('/api/links', linkRoute);
 app.use(errorHandler);
+
+// use the client app
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// render client for any path
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../client/dist/index.html')));
 
 app.listen(port, (req, res) => {
     console.log(`Server is running on port ${port}`);
